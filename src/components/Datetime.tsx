51feaebf6bd601c0b3e86219d7c5c5fbd3fc1a0b
Kris Hu <i@krishu.moe>
2024-02-07 12:21:31 +0000

    feat(locale): set lang to zh, tz to +8

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -52,11 +52,13 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     year: "numeric",
     month: "short",
     day: "numeric",
+    timeZone: LOCALE.timeZone,
   });
 
   const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
     hour: "2-digit",
     minute: "2-digit",
+    timeZone: LOCALE.timeZone,
   });
 
   return (
